de26904b4a2890ff7c4102534c63c76ec22c9065
Mahmed amer <67114727+Mahmedamer@users.noreply.github.com>
2020-10-28 14:35:30 +0200

    My Solution For problem 7

---
+++ Week2Online/Problem7.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+#define n 10
+int ReplaceAll(int arr[], int oldv, int newv) 
+{
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == oldv)
+		{
+			arr[i] = newv;
+			count++;
+		}
+	}
+	return count;
+}
+int main() 
+{
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Enter The Value "<< i <<" : ";
+		cin >> arr[i];
+	}
+	int oldv, newv;
+	cout << "Enter old Value : ";
+	cin >> oldv;
+	cout << "Enter New Value : ";
+	cin >> newv;
+	int count = ReplaceAll(arr, oldv, newv);
+	cout << "Number of Occurances " << count << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
